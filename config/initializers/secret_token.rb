eefe80612c6035e7a1d46c87c1d9979bf7beb2e6
Laiz Cristina <laiz_braga@fantasticatecnologia.com.br>
2012-08-03 14:30:24 -0300

    novo projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estoque::Application.config.secret_token = 'c13ae4cb6790cb1e50a6f0138577de4bff377fdb7e7754be018fb9ab065b4f5f82ca198c0bf326ad8be824ecca0f36daecc970942aa6af01a0557e5bd5d69f0d'
